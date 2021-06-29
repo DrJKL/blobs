a500576c2321102b7d4572cbd4231227f2e1e6b4
Alexander Brown <DrJKL0424@gmail.com>
2021-06-28 23:59:15 -0700

    Do more things.

--- src/components/GameCanvas.vue
+++ src/components/GameCanvas.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="game-canvas-container">
-    <h1>THIS</h1>
-    <canvas ref="gameCanvas" width="600" height="400">Nope, No Canvas.</canvas>
+    <h1>THIS {{ num }}</h1>
+    <canvas ref="gameCanvas" class="game-cangas" width="600" height="400"
+      >Nope, No Canvas.</canvas
+    >
   </div>
 </template>
 
 <script lang="ts">
 import { Vue } from 'vue-class-component';
-import {Ref} from 'vue-property-decorator';
+import { Ref } from 'vue-property-decorator';
+import { Subscription, timer } from 'rxjs';
 export default class ComponentName extends Vue {
   @Ref() gameCanvas!: HTMLCanvasElement;
+
+  num = 0;
+
+  canvasTimer?: Subscription;
 
   mounted() {
     const realCanvas = this.gameCanvas;
@@ -20,8 +27,17 @@ export default class ComponentName extends Vue {
     const ctx = realCanvas.getContext('2d');
     if (ctx) {
       ctx.fillStyle = '#F0F';
-      ctx.fillRect(0,0,realCanvas.width, realCanvas.height)
+      ctx.fillRect(0, 0, realCanvas.width, realCanvas.height);
     }
+
+    this.canvasTimer = timer(0, 500).subscribe((i) => {
+      if (ctx) {
+        ctx.fillStyle = '#' + Math.random().toString(16).substr(2, 6);
+        ctx?.fillRect(0, 0, realCanvas.width, realCanvas.height);
+      }
+      this.num = i;
+      console.log(i);
+    });
   }
 }
 </script>
