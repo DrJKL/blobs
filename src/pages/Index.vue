c222d5575312ee3e66aae03d12c40645dcd28b93
Alexander Brown <DrJKL0424@gmail.com>
2021-07-04 22:39:51 -0700

    Delete Skeleton Component

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,13 +5,12 @@
 </template>
 
 <script lang="ts">
-import ExampleComponent from 'components/ClassComponent.vue';
 import GameCanvas from 'components/GameCanvas.vue';
 import BuddiesComponent from 'components/BuddiesComponent.vue';
 import { Vue, Options } from 'vue-class-component'
 
 @Options({
-  components: { ExampleComponent, GameCanvas, BuddiesComponent }
+  components: { GameCanvas, BuddiesComponent }
 })
 export default class PageIndex extends Vue {
   
