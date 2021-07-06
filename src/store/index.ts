6700f30399dc589926a4d0bc80433864f1e0b9af
Alexander Brown <DrJKL0424@gmail.com>
2021-07-06 00:16:41 -0700

    Delete StateInterface

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,36 +7,20 @@ import {
   useStore as vuexUseStore,
 } from 'vuex'
 
-/*
- * If not building with SSR mode, you can
- * directly export the Store instantiation;
- *
- * The function below can be async too; either use
- * async/await or return a Promise which resolves
- * with the Store instance.
- */
-
-export interface StateInterface {
-  // Define your own store structure, using submodules if needed
-  // example: ExampleStateInterface;
-  // Declared as unknown to avoid linting issue. Best to strongly type as per the line above.
-  example: unknown,
-}
-
 // provide typings for `this.$store`
 declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
-    $store: VuexStore<StateInterface>
+    $store: VuexStore<unknown>
   }
 }
 
 // provide typings for `useStore` helper
-export const storeKey: InjectionKey<VuexStore<StateInterface>> = Symbol('vuex-key')
+export const storeKey: InjectionKey<VuexStore<unknown>> = Symbol('vuex-key')
 
 export let storeInstance: Store<unknown>;
 
 export default store(function (/* { ssrContext } */) {
-  const store = createStore<StateInterface>({
+  const store = createStore({
     modules: {
     },
 
