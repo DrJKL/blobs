fb2ba9ab17bc087306fe3d03ea093a2cb5d1baba
Alexander Brown <DrJKL0424@gmail.com>
2021-07-15 00:15:14 -0700

    Formatters are both fantastic and evil

--- src/buddies/buddy.ts
+++ src/buddies/buddy.ts
@@ -1,10 +1,10 @@
 import { Posse } from './posse';
 import SketchStore from 'src/store/sketch_store';
 import {
-    AvoidBordersDrive,
-    AvoidOtherBuddiesDrive,
-    Drive,
-    MoveRandomly,
+  AvoidBordersDrive,
+  AvoidOtherBuddiesDrive,
+  Drive,
+  MoveRandomly,
 } from './drives';
 import p5, { Vector, Color } from 'p5';
 import { analogous, complement } from 'src/helpers/color_utils';
@@ -12,325 +12,339 @@ import BuddyStore from '../store/buddy_store';
 import Queue from 'src/helpers/queue';
 
 function randomColor(p: p5): Color {
-    return p.color(p.random(360), p.random(40, 100), p.random(80, 100));
+  return p.color(p.random(360), p.random(40, 100), p.random(80, 100));
 }
 
 function defaultDrives() {
-    return [
-        new AvoidBordersDrive(),
-        new AvoidOtherBuddiesDrive(),
-        new MoveRandomly(),
-    ];
+  return [
+    new AvoidBordersDrive(),
+    new AvoidOtherBuddiesDrive(),
+    new MoveRandomly(),
+  ];
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function ogColors(p: p5) {
-    return [
-        p.color(316, 58, 99, 0.07),
-        p.color(283, 50, 91, 0.07),
-        p.color(257, 45.1, 100, 0.07),
-        p.color(231, 41.8, 91, 0.07),
-    ];
+  return [
+    p.color(316, 58, 99, 0.07),
+    p.color(283, 50, 91, 0.07),
+    p.color(257, 45.1, 100, 0.07),
+    p.color(231, 41.8, 91, 0.07),
+  ];
 }
 interface Renderable {
-    render(): void;
-    renderDebug(): void;
+  render(): void;
+  renderDebug(): void;
 }
 
 export class Segment implements Renderable {
-    constructor(
-        readonly p: p5,
-        public position: Vector,
-        public color: Color = randomColor(p)
-    ) {
-        this.color.setAlpha(0.27);
-    }
+  constructor(
+    readonly p: p5,
+    public position: Vector,
+    public color: Color = randomColor(p)
+  ) {
+    this.color.setAlpha(0.27);
+  }
 
-    render() {
-        this.p.push();
-        this.p.noStroke();
-        this.p.fill(this.color);
-        this.p.translate(this.position.x, this.position.y);
-        this.p.circle(0, 0, this.position.z);
-        this.p.pop();
-    }
+  render() {
+    this.p.push();
+    this.p.noStroke();
+    this.p.fill(this.color);
+    this.p.translate(this.position.x, this.position.y);
+    this.p.circle(0, 0, this.position.z);
+    this.p.pop();
+  }
 
-    renderDebug() {
-        const debugLayer = SketchStore.debugGraphic;
-        const sketch = SketchStore.mainSketch;
-        if (!debugLayer || !sketch || !SketchStore.debugOn) return;
-        debugLayer.push();
+  renderDebug() {
+    const debugLayer = SketchStore.debugGraphic;
+    const sketch = SketchStore.mainSketch;
+    if (!debugLayer || !sketch || !SketchStore.debugOn) return;
+    debugLayer.push();
 
-        const px = this.position.x;
-        const py = this.position.y;
+    const px = this.position.x;
+    const py = this.position.y;
 
-        const mx = sketch.mouseX - px;
-        const my = sketch.mouseY - py;
+    const mx = sketch.mouseX - px;
+    const my = sketch.mouseY - py;
 
-        debugLayer.stroke(0, 0, 100, 1);
-        debugLayer.strokeWeight(1);
-        debugLayer.noFill();
+    debugLayer.stroke(0, 0, 100, 1);
+    debugLayer.strokeWeight(1);
+    debugLayer.noFill();
 
-        const directAngle = sketch.atan2(my, mx) - sketch.atan2(py, px);
+    const directAngle = sketch.atan2(my, mx) - sketch.atan2(py, px);
 
-        debugLayer.translate(px, py);
-        debugLayer.rotate(directAngle);
+    debugLayer.translate(px, py);
+    debugLayer.rotate(directAngle);
 
-        debugLayer.line(0, 0, 30, 30);
+    debugLayer.line(0, 0, 30, 30);
 
-        debugLayer.pop();
-        // Nothing yet.
-    }
+    debugLayer.pop();
+    // Nothing yet.
+  }
 
-    copy(velocity: Vector, size: number, color: Color) {
-        const kidPosition = Vector.add(this.position, velocity);
-        kidPosition.z = size;
-        return new Segment(this.p, kidPosition, color);
-    }
+  copy(velocity: Vector, size: number, color: Color) {
+    const kidPosition = Vector.add(this.position, velocity);
+    kidPosition.z = size;
+    return new Segment(this.p, kidPosition, color);
+  }
 }
 
 export class Egg implements Renderable {
-    age = 0;
-    hatchAge = 500;
-    expirationAge = 1000;
-    outline: Color;
+  age = 0;
+  hatchAge = 500;
+  expirationAge = 1000;
+  outline: Color;
 
-    constructor(
-        readonly p: p5,
-        readonly position: Vector,
-        readonly color: Color = randomColor(p),
-        readonly generation = 1
-    ) {
-        this.outline = complement(p, color)[1];
-    }
+  constructor(
+    readonly p: p5,
+    readonly position: Vector,
+    readonly color: Color = randomColor(p),
+    readonly generation = 1
+  ) {
+    this.outline = complement(p, color)[1];
+  }
 
-    update() {
-        this.age++;
-    }
+  update() {
+    this.age++;
+  }
 
-    draw() {
-        this.render();
-        this.renderDebug();
-    }
+  draw() {
+    this.render();
+    this.renderDebug();
+  }
 
-    render() {
-        const p = SketchStore.mainGraphic;
-        if (!p) {
-            return;
-        }
-        p.push();
-        p.fill(this.color);
-        p.stroke(this.outline);
-        p.ellipse(this.position.x, this.position.y, this.width, this.height);
-        p.pop();
+  render() {
+    const p = SketchStore.mainGraphic;
+    if (!p) {
+      return;
     }
+    p.push();
+    p.fill(this.color);
+    p.stroke(this.outline);
+    p.ellipse(this.position.x, this.position.y, this.width, this.height);
+    p.pop();
+  }
 
-    renderDebug() {
-        const d = SketchStore.debugGraphic;
-        if (!d || !SketchStore.isDebug) {
-            return;
-        }
-        d.fill(0, 0, 100, 1);
-        d.textSize(32);
-        d.text(this.debugText, this.position.x, this.position.y - 20);
+  renderDebug() {
+    const d = SketchStore.debugGraphic;
+    if (!d || !SketchStore.isDebug) {
+      return;
     }
+    d.fill(0, 0, 100, 1);
+    d.textSize(32);
+    d.text(this.debugText, this.position.x, this.position.y - 20);
+  }
 
-    private get debugText() {
-        return this.expired
-            ? `<${this.age}>`
-            : this.ready
-                ? `(${this.age})`
-                : `[${this.age}]`;
-    }
+  private get debugText() {
+    return this.expired
+      ? `<${this.age}>`
+      : this.ready
+      ? `(${this.age})`
+      : `[${this.age}]`;
+  }
 
-    private get size() {
-        const base = this.expired
-            ? 10
-            : this.ready
-                ? this.expirationAge - this.age
-                : this.age;
-        return 10 * Math.pow(base, 0.3);
-    }
-    private get width() {
-        return (
-            (this.size * 0.8) +
-            (this.almostReady ? this.p.sin(((this.age + 50) / 100) * (50 / this.timeUntilReady) * this.p.TWO_PI) * 5 : 0)
-        );
-    }
-    private get height() {
-        return (
-            this.size +
-            (this.almostReady ? this.p.sin((this.age / 100) * (100 / this.timeUntilReady) * this.p.TWO_PI) * 4 : 0)
-        );
-    }
+  private get size() {
+    const base = this.expired
+      ? 10
+      : this.ready
+      ? this.expirationAge - this.age
+      : this.age;
+    return 10 * Math.pow(base, 0.3);
+  }
+  private get width() {
+    return (
+      this.size * 0.8 +
+      (this.almostReady
+        ? this.p.sin(
+            ((this.age + 50) / 100) * (50 / this.timeUntilReady) * this.p.TWO_PI
+          ) * 5
+        : 0)
+    );
+  }
+  private get height() {
+    return (
+      this.size +
+      (this.almostReady
+        ? this.p.sin(
+            (this.age / 100) * (100 / this.timeUntilReady) * this.p.TWO_PI
+          ) * 4
+        : 0)
+    );
+  }
 
-    private get almostReady() {
-        return this.timeUntilReady < 200;
-    }
+  private get almostReady() {
+    return this.timeUntilReady < 200;
+  }
 
-    private get timeUntilReady() {
-        return Math.abs(this.hatchAge - this.age);
-    }
+  private get timeUntilReady() {
+    return Math.abs(this.hatchAge - this.age);
+  }
 
-    get ready() {
-        return this.age >= this.hatchAge;
-    }
+  get ready() {
+    return this.age >= this.hatchAge;
+  }
 
-    get expired() {
-        return this.age >= this.expirationAge;
-    }
+  get expired() {
+    return this.age >= this.expirationAge;
+  }
 
-    hatch(p: p5) {
-        return new Buddy(p, this.position, analogous(this.p, this.color));
-    }
+  hatch(p: p5) {
+    return new Buddy(
+      p,
+      this.position,
+      analogous(this.p, this.color),
+      this.generation
+    );
+  }
 }
 
 export class Buddy implements Renderable {
-    age = 0;
-    alive = true;
-    velocity = Vector.random2D();
-    acceleration = new Vector();
-    size = 15;
-    get maxSpeed() {
-        return this.size / 4;
-    }
-    maxLength = 100;
-    body: Queue<Segment>;
-    secondaryColor: Color;
+  age = 0;
+  alive = true;
+  velocity = Vector.random2D();
+  acceleration = new Vector();
+  size = 15;
+  get maxSpeed() {
+    return this.size / 4;
+  }
+  maxLength = 25;
+  body: Queue<Segment>;
+  secondaryColor: Color;
 
-    drives: Drive[] = defaultDrives();
+  drives: Drive[] = defaultDrives();
 
-    stomach: Array<unknown> = [];
-    stomachCapacity = 3;
-    calories = 100;
+  stomach: Array<unknown> = [];
+  stomachCapacity = 3;
+  calories = 100;
 
-    constructor(
-        readonly p: p5,
-        pos: Vector,
-        readonly colors: Color[] = analogous(p, randomColor(p)),
-        readonly generation = 1
-    ) {
-        this.secondaryColor = randomColor(p);
-        this.secondaryColor.setAlpha(0.2);
+  constructor(
+    readonly p: p5,
+    pos: Vector,
+    readonly colors: Color[] = analogous(p, randomColor(p)),
+    readonly generation = 1
+  ) {
+    this.secondaryColor = randomColor(p);
+    this.secondaryColor.setAlpha(0.2);
 
-        pos.z = this.velocity.mag();
-        this.body = new Queue<Segment>();
-        this.body.enqueue(new Segment(p, pos.copy(), this.colors[0]));
-    }
+    pos.z = this.velocity.mag();
+    this.body = new Queue<Segment>();
+    this.body.enqueue(new Segment(p, pos.copy(), this.colors[0]));
+  }
 
-    get position() {
-        return this.body.peekTail()?.position ?? new Vector();
-    }
+  get position() {
+    return this.body.peekTail()?.position ?? new Vector();
+  }
 
-    get allDone() {
-        return this.body.size() === 1;
-    }
+  get allDone() {
+    return this.body.size() === 1;
+  }
 
-    update(posse: Posse) {
-        this.age++;
-        if (this.age > BuddyStore.ageLimit) {
-            this.alive = false;
-        }
-
-        this.move(posse.buddies);
-
+  update(posse: Posse) {
+    this.age++;
+    if (this.age > BuddyStore.ageLimit) {
+      this.alive = false;
     }
 
-    draw() {
-        this.render();
-        this.renderDebug();
+    this.move(posse.buddies);
+  }
+
+  draw() {
+    this.render();
+    this.renderDebug();
+  }
+
+  render() {
+    this.p.noStroke();
+    const bodySegs = this.body.entries();
+    for (let i = 0; i < bodySegs.length - 1; i++) {
+      const pos = bodySegs[i];
+      pos.render();
+      pos.position.z *= 0.99;
+    }
+    if (this.alive) {
+      this.drawForce(this.velocity, this.position);
+    }
+  }
+  renderDebug() {
+    const d = SketchStore.debugGraphic;
+    if (!d || !SketchStore.isDebug) {
+      return;
+    }
+    d.fill(0, 0, 100, 1);
+    d.textSize(32);
+    d.text(
+      `${this.age} - ${this.generation}`,
+      this.position.x,
+      this.position.y - 20
+    );
+  }
+
+  move(buddies: Buddy[]) {
+    for (const drive of this.drives) {
+      for (const goal of drive.getGoals(this.p, this, buddies)) {
+        this.steer(goal);
+      }
+      for (const force of drive.getForces(this.p, this, buddies)) {
+        this.applyForce(force);
+      }
+    }
+    this.changeVelocity();
+  }
+
+  applyForce(force: Vector) {
+    force.div(this.size);
+    this.acceleration.add(force);
+  }
+
+  drawForce(force: Vector, pos: Vector) {
+    const forceVis = p5.Vector.mult(force, 10).add(pos);
+    this.p.stroke(this.secondaryColor);
+    this.p.strokeWeight(4);
+    this.p.noFill();
+    this.p.line(pos.x, pos.y, forceVis.x, forceVis.y);
+  }
+
+  changeVelocity() {
+    this.velocity.add(this.acceleration).limit(this.maxSpeed);
+
+    if (!this.alive) {
+      const toPrune = this.body.size() / 10;
+      for (let i = 0; i < toPrune; i++) {
+        this.body.dequeue();
+      }
+      return;
     }
 
-    render() {
-        this.p.noStroke();
-        const bodySegs = this.body.entries();
-        for (let i = 0; i < bodySegs.length - 1; i++) {
-            const pos = bodySegs[i];
-            // const next = this.body[i + 1];
-            pos.render();
-            // if (i % 5 === 0) pos.renderDebug();
-            pos.position.z *= 0.99;
-        }
-        if (this.alive) {
-            this.drawForce(this.velocity, this.position);
+    while (this.body.size() >= this.maxLength) {
+      this.body.dequeue();
     }
+    const head = this.body.peekTail();
+    if (!head) {
+      return;
     }
-    renderDebug() {
-        const d = SketchStore.debugGraphic;
-        if (!d || !SketchStore.isDebug) {
-            return;
-        }
-        d.fill(0, 0, 100, 1);
-        d.textSize(32);
-        d.text(`${this.age}`, this.position.x, this.position.y - 20);
-    }
+    const segColorIndex = Math.floor(this.age / 10) % this.colors.length;
+    const segColor = this.colors[segColorIndex];
+    const newSegment = head.copy(
+      this.velocity,
+      (this.size * 2) / this.p.max(this.velocity.mag(), 1),
+      segColor
+    );
+    this.body.enqueue(newSegment);
+    this.acceleration.mult(0);
+  }
 
-    move(buddies: Buddy[]) {
-        for (const drive of this.drives) {
-            for (const goal of drive.getGoals(this.p, this, buddies)) {
-                this.steer(goal);
-            }
-            for (const force of drive.getForces(this.p, this, buddies)) {
-                this.applyForce(force);
-            }
-        }
-        this.changeVelocity();
-    }
+  steer(desired: Vector) {
+    desired.sub(this.velocity);
+    desired.limit(3);
+    this.applyForce(desired);
+  }
 
-    applyForce(force: Vector) {
-        force.div(this.size);
-        this.acceleration.add(force);
-    }
-
-    drawForce(force: Vector, pos: Vector) {
-        const forceVis = p5.Vector.mult(force, 10).add(pos);
-        this.p.stroke(this.secondaryColor);
-        this.p.strokeWeight(4);
-        this.p.noFill();
-        this.p.line(pos.x, pos.y, forceVis.x, forceVis.y);
-    }
-
-    changeVelocity() {
-        this.velocity.add(this.acceleration).limit(this.maxSpeed);
-
-        if (!this.alive) {
-            const toPrune = this.body.size() / 10;
-            for (let i = 0; i < toPrune; i++) {
-                this.body.dequeue();
-            }
-            return;
-        }
-
-        while (this.body.size() >= this.maxLength) {
-            this.body.dequeue();
-        }
-        const head = this.body.peekTail();
-        if (!head) {
-            return;
-        }
-        const segColorIndex = Math.floor(this.age / 10) % this.colors.length;
-        const segColor = this.colors[segColorIndex];
-        const newSegment = head.copy(
-            this.velocity,
-            (this.size * 2) / this.p.max(this.velocity.mag(), 1),
-            segColor
-        );
-        this.body.enqueue(newSegment);
-        this.acceleration.mult(0);
-    }
-
-    steer(desired: Vector) {
-        desired.sub(this.velocity);
-        desired.limit(3);
-        this.applyForce(desired);
-    }
-
-    toEgg() {
-        return new Egg(
-            this.p,
-            this.position,
-            this.colors[this.p.random(this.colors.length)],
-            this.generation + 1
-        );
-    }
+  toEgg() {
+    return new Egg(
+      this.p,
+      this.position,
+      this.colors[this.p.random(this.colors.length)],
+      this.generation + 1
+    );
+  }
 }
--- src/buddies/drives.ts
+++ src/buddies/drives.ts
@@ -3,113 +3,113 @@ import { Buddy, Egg } from './buddy';
 import p5, { Vector } from 'p5';
 
 export interface Drive {
-    getGoals(p: p5, buddy: Buddy, buddies: Buddy[]): Vector[];
-    getForces(p: p5, buddy: Buddy, buddies: Buddy[]): Vector[];
+  getGoals(p: p5, buddy: Buddy, buddies: Buddy[]): Vector[];
+  getForces(p: p5, buddy: Buddy, buddies: Buddy[]): Vector[];
 }
 export class XDrive implements Drive {
-    goals: Vector[];
-    constructor(_p: p5, goals: Vector[]) {
-        this.goals = [...goals];
-    }
-    getGoals(_p: p5, buddy: Buddy, _buddies: Buddy[]) {
-        return this.goals.map((goal) => Vector.sub(goal, buddy.position));
-    }
-    getForces(_p: p5, _buddy: Buddy, _buddies: Buddy[]) {
-        return [];
-    }
+  goals: Vector[];
+  constructor(_p: p5, goals: Vector[]) {
+    this.goals = [...goals];
+  }
+  getGoals(_p: p5, buddy: Buddy, _buddies: Buddy[]) {
+    return this.goals.map((goal) => Vector.sub(goal, buddy.position));
+  }
+  getForces(_p: p5, _buddy: Buddy, _buddies: Buddy[]) {
+    return [];
+  }
 }
 
 export class AvoidBordersDrive implements Drive {
-    getGoals(p: p5, buddy: Buddy, _buddies: Buddy[]) {
-        const position = buddy.position;
-        if (!position) {
-            return [];
-        }
-        const goals: Vector[] = [];
-        const offset = 25;
-        if (position.x < offset) {
-            goals.push(p.createVector(buddy.maxSpeed, buddy.velocity.y));
-        }
-        if (position.x > p.width - offset) {
-            goals.push(p.createVector(-buddy.maxSpeed, buddy.velocity.y));
-        }
-        if (position.y < offset) {
-            goals.push(p.createVector(buddy.velocity.x, buddy.maxSpeed));
-        }
-        if (position.y > p.height - offset) {
-            goals.push(p.createVector(buddy.velocity.x, -buddy.maxSpeed));
-        }
-        return goals;
+  getGoals(p: p5, buddy: Buddy, _buddies: Buddy[]) {
+    const position = buddy.position;
+    if (!position) {
+      return [];
     }
-    getForces(_p: p5, _buddy: Buddy, _buddies: Buddy[]) {
-        return [];
+    const goals: Vector[] = [];
+    const offset = 25;
+    if (position.x < offset) {
+      goals.push(p.createVector(buddy.maxSpeed, buddy.velocity.y));
     }
+    if (position.x > p.width - offset) {
+      goals.push(p.createVector(-buddy.maxSpeed, buddy.velocity.y));
+    }
+    if (position.y < offset) {
+      goals.push(p.createVector(buddy.velocity.x, buddy.maxSpeed));
+    }
+    if (position.y > p.height - offset) {
+      goals.push(p.createVector(buddy.velocity.x, -buddy.maxSpeed));
+    }
+    return goals;
+  }
+  getForces(_p: p5, _buddy: Buddy, _buddies: Buddy[]) {
+    return [];
+  }
 }
 
 export class AvoidOtherBuddiesDrive implements Drive {
-    getGoals(_p: p5, _buddy: Buddy, _buddies: Buddy[]) {
-        return [];
-    }
-    getForces(p: p5, buddy: Buddy, buddies: Buddy[]) {
-        const forces = [];
-        for (const other of buddies) {
-            if (buddy === other) {
-                continue;
-            }
-            const otherBody = other.body.entries();
-            for (let i = 0; i < otherBody.length; i += 4) {
-                const segment = otherBody[i];
-              forces.push(repel(p, segment.position, buddy.position));
-          }
+  getGoals(_p: p5, _buddy: Buddy, _buddies: Buddy[]) {
+    return [];
+  }
+  getForces(p: p5, buddy: Buddy, buddies: Buddy[]) {
+    const forces = [];
+    for (const other of buddies) {
+      if (buddy === other) {
+        continue;
       }
-      return forces;
+      const otherBody = other.body.entries();
+      for (let i = 0; i < otherBody.length; i += 5) {
+        const segment = otherBody[i];
+        forces.push(repel(p, segment.position, buddy.position));
+      }
+    }
+    return forces;
   }
 }
 
 export class AvoidEggsDrive implements Drive {
-    getGoals(_p: p5, _buddy: Buddy, _buddies: Buddy[]) {
-        return [];
+  getGoals(_p: p5, _buddy: Buddy, _buddies: Buddy[]) {
+    return [];
+  }
+  getForces(p: p5, buddy: Buddy, buddies: Buddy[]) {
+    const forces = [];
+    const eggs: Egg[] = [];
+    for (const other of eggs) {
+      forces.push(repel(p, other.position, buddy.position));
     }
-    getForces(p: p5, buddy: Buddy, buddies: Buddy[]) {
-        const forces = [];
-        const eggs: Egg[] = [];
-        for (const other of eggs) {
-            forces.push(repel(p, other.position, buddy.position));
-        }
-        return forces;
-    }
+    return forces;
+  }
 }
 
 export class MoveRandomly implements Drive {
-    ticks: number;
-    constructor() {
-        this.ticks = 0;
-    }
-    getGoals(_p: p5, _buddy: Buddy, _buddies: Buddy[]) {
-        return [];
-    }
-    getForces(_p: p5, _buddy: Buddy, _buddies: Buddy[]) {
-        this.ticks++;
-        const forces = [];
-        // if (this.ticks % 1 == 0) {
-        forces.push(Vector.random2D());
-        // }
-        return forces;
-    }
+  ticks: number;
+  constructor() {
+    this.ticks = 0;
+  }
+  getGoals(_p: p5, _buddy: Buddy, _buddies: Buddy[]) {
+    return [];
+  }
+  getForces(_p: p5, _buddy: Buddy, _buddies: Buddy[]) {
+    this.ticks++;
+    const forces = [];
+    // if (this.ticks % 1 == 0) {
+    forces.push(Vector.random2D());
+    // }
+    return forces;
+  }
 }
 
 function repel(p: p5, buddy: Vector, otherBuddy: Vector) {
-    const dir = Vector.sub(buddy, otherBuddy);
+  const dir = Vector.sub(buddy, otherBuddy);
 
-    const d = dir.mag();
-    if (d > 500) {
-        return p.createVector();
-    }
-    const force = (-1 * 100) / (d * d);
+  const d = dir.mag();
+  if (d > 500) {
+    return p.createVector();
+  }
+  const force = (-1 * 100) / (d * d);
 
-    dir.normalize();
-    if (force && !isNaN(force) && isFinite(force)) {
-        dir.mult(force);
-    }
-    return dir;
+  dir.normalize();
+  if (force && !isNaN(force) && isFinite(force)) {
+    dir.mult(force);
+  }
+  return dir;
 }
